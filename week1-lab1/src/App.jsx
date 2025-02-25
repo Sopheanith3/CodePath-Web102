bd41302e8be07ec03bcfc514dd79fdee3a74e0f8
Sopheanith3 <nsopheanith3@gmail.com>
2025-02-24 17:17:52 -0800

    adding week1

---
+++ week1-lab1/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Calendar from './components/Calendar'
+
+const App = () => {
+  
+  return (
+    <div className="App">
+      <h1>Itinerary for 7 Days in Chicago 🏙️</h1>
+      <h4>One week in Chicago is wonderful. If you want to get to know the city and see all the sights,then spending 7 days in Chicago is perfect!</h4>
+       <Calendar />
+    </div>
+  )
+}
+
+export default App
