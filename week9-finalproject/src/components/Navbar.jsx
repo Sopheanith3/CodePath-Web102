8afe73d9f15986e11f00743e8e5a091f34ad34eb
Sopheanith3 <nsopheanith3@gmail.com>
2025-04-25 01:10:36 -0700

    align the nav bar

--- week9-finalproject/src/components/Navbar.jsx
+++ week9-finalproject/src/components/Navbar.jsx
@@ -5,41 +5,30 @@ import './Navbar.css';
 function Navbar() {
   return (
     <header className="navbar">
-      <div className="navbar-left">
-      <Link to="/" className="navbar-logo">
-        <div className="champions-logo">
-          <img 
-            src="/src/assets/uefa-logo.png" 
-            alt="Champions League" 
-            width="32" 
-            height="32" 
-            style={{ marginRight: '8px' }}
-          />
-        </div>
-        <span className="navbar-logo-text">Champions League Hub</span>
-      </Link>
-        
-        <div className="navbar-search">
-          <span className="navbar-search-icon">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" width="18" height="18">
-              <path fill="currentColor" d="M15.59,13.91l2.78,2.69a1.25,1.25,0,1,1-1.74,1.8l-2.82-2.73a8,8,0,1,1,1.77-1.78ZM8,14A6,6,0,1,0,2,8,6,6,0,0,0,8,14Z"></path>
-            </svg>
-          </span>
-          <input
-            type="text"
-            className="navbar-search-input"
-            placeholder="Search Champions League Hub"
-          />
-        </div>
+  <div className="navbar-left">
+    <Link to="/" className="navbar-logo">
+      <div className="champions-logo">
+        <img 
+          src="/src/assets/uefa-logo.png" 
+          alt="Champions League" 
+          width="32" 
+          height="32"
+        />
       </div>
-      
-      <div className="navbar-right">
-        <Link to="/create">
-          <button className="navbar-button create-post-button">Create Post</button>
-        </Link>
-        <button className="navbar-button login-button">Log In</button>
-      </div>
-    </header>
+    </Link>
+  </div>
+
+  <div className="navbar-center">
+    <span className="navbar-logo-text">Champions League Hub</span>
+  </div>
+
+  <div className="navbar-right">
+    <Link to="/create">
+      <button className="navbar-button create-post-button">Create Post</button>
+    </Link>
+    <button className="navbar-button login-button">Log In</button>
+  </div>
+</header>
   );
 }
 
