de99a994356e96e34702e319609821e8a9ec0966
Sopheanith3 <nsopheanith3@gmail.com>
2025-04-25 19:08:47 -0700

    fixing the bar

--- week9-finalproject/src/components/Navbar.jsx
+++ week9-finalproject/src/components/Navbar.jsx
@@ -5,30 +5,34 @@ import './Navbar.css';
 function Navbar() {
   return (
     <header className="navbar">
-    <div className="navbar-left">
-      <Link to="/" className="navbar-logo">
-        <div className="champions-logo">
-          <img 
-            src="/src/assets/uefa-logo.png" 
-            alt="Champions League" 
-            width="32" 
-            height="32"
-          />
-        </div>
-      </Link>
-    </div>
+      <div className="navbar-left">
+        <Link to="/" className="navbar-logo">
+          <div className="champions-logo">
+            <img 
+              src="/src/assets/uefa-logo.png" 
+              alt="Champions League" 
+              width="32" 
+              height="32"
+              onError={(e) => {
+                console.error("Logo image failed to load");
+                e.target.style.display = 'none';
+              }}
+            />
+          </div>
+        </Link>
+      </div>
 
-    <div className="navbar-center">
-      <span className="navbar-logo-text">Champions League Hub</span>
-    </div>
+      <div className="navbar-center">
+        <span className="navbar-logo-text">Champions League Hub</span>
+      </div>
 
-    <div className="navbar-right">
-      <Link to="/create">
-        <button className="navbar-button create-post-button">Create Post</button>
-      </Link>
-      <button className="navbar-button login-button">Log In</button>
-    </div>
-  </header>
+      <div className="navbar-right">
+        <Link to="/create">
+          <button className="navbar-button create-post-button">Create Post</button>
+        </Link>
+        <button className="navbar-button login-button">Log In</button>
+      </div>
+    </header>
   );
 }
 
